48543c8d00fbb51f3ff0910bbde394e45d1096ec
Abdulrahman Khalil <20816524+askhalil@users.noreply.github.com>
2024-10-06 19:21:35 +0300

    python

--- super.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class super
-{
-    private int x { get; set; }
-    public virtual void print()
-    {
-        Console.WriteLine("Super.Print()");
-    }
---
+++ super.py
@@ -0,0 +1,2 @@
+Def super:
+Print
